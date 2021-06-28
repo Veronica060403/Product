206490bdf91a2b962201c97cc3b523f7dc9102ad
Veronica060403 <84276638+Veronica060403@users.noreply.github.com>
2021-06-28 14:18:58 +0530

    Product
    cpp product of numbers

---
+++ product.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+main()
+{
+    float a,b,Product;
+    cout<<"Enter first number:";
+    cin>>a;
+    cout<<"Enter second number:";
+    cin>>b;
+    Product=a*b;
+    cout<<"Product of two numbers = "<<Product;
+}
